9ad3722f0b16e83de1e7125d784a185339079add
simon@syhol.io <simon@syhol.io>
2024-05-10 16:01:39 +0100

    split out scripts

---
+++ dot_config/zsh/prompt-p10k.zsh
@@ -0,0 +1,6 @@
+# Load the prompt 
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+
