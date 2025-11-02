c43fa82a016abf65c7a5cb8e70fe6c89a8cbda7e
Simon Holloway <simon@syhol.io>
2025-11-02 14:42:47 +0000

    2025-11-02T14:42:47Z

--- dot_config/shell/fzf.sh
+++ dot_config/shell/fzf.sh
@@ -11,17 +11,25 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
   --no-scrollbar --preview-window right,border-left \
   --height=50% \
 "
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+
+# jethrokuan/fzf (fish)
 export FZF_COMPLETE=2
 export FZF_PREVIEW_FILE_CMD="bat --style=plain --paging=never --color=always --wrap=character"
 export FZF_PREVIEW_DIR_CMD="eza --color=always --icons always --group-directories-first --all --width 1"
 export FZF_OPEN_COMMAND="fd --type f --hidden --exclude .git --exclude node_modules \$dir"
-export FZF_ENABLE_OPEN_PREVIEW=1
 export FZF_CD_COMMAND="fd --type d --hidden --exclude .git --exclude node_modules \$dir"
 export FZF_CD_WITH_HIDDEN_COMMAND="fd --type d --hidden --exclude .git --exclude node_modules . ~"
 export FZF_CD_OPTS="--preview '$FZF_PREVIEW_DIR_CMD {}'"
 export FZF_CD_WITH_HIDDEN_OPTS="--preview '$FZF_PREVIEW_DIR_CMD {}'"
 export FZF_REVERSE_ISEARCH_OPTS="--preview 'echo {2..} | $FZF_PREVIEW_FILE_CMD -l bash' --preview-window 1,bottom,wrap,border-top"
-export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --preview '$FZF_PREVIEW_DIR_CMD {2..}'"
+export FZF_ENABLE_OPEN_PREVIEW=1
+
+# Core FZF key bindings
 export FZF_ALT_C_OPTS="$FZF_DEFAULT_OPTS --preview '$FZF_PREVIEW_DIR_CMD {}'"
 export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS --preview '$FZF_PREVIEW_DIR_CMD {}'"
 export FZF_CTRL_R_OPTS="$FZF_DEFAULT_OPTS --preview 'echo {2..} | $FZF_PREVIEW_FILE_CMD -l bash' --preview-window 1,bottom,wrap,border-top"
+
+# Yazi and Zoxide
+export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --preview '$FZF_PREVIEW_DIR_CMD {2..}'"
+export YAZI_ZOXIDE_OPTS="--no-border --no-cycle --preview-window right,border-left --preview '$FZF_PREVIEW_DIR_CMD {2..}'"
