85ad9af5b1470809313b0b3a86468a06774ad0b0
Simon Holloway <simon@syhol.io>
2026-06-27 00:53:00 +0100

    2026-06-26T23:53:00Z

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,5 @@ if status is-interactive
     mise activate fish | source
     zoxide init fish --cmd cd | source
     starship init fish | source
+    test -n "$FISH_THEME"; and fish_config theme choose "$FISH_THEME"
 end
